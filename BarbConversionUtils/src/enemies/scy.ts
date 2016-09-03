15ea25c955f0b0595f2774e685e4fb728a4a0153
bpandola <brian.pandola@philips.com>
2016-09-02 23:49:57 -0700

    Initial Commit

---
+++ BarbConversionUtils/src/enemies/scy.ts
@@ -0,0 +1,29 @@
+﻿/// <reference path="enemy.ts" />
+namespace Barbarian.Enemies {
+
+    export class Scythe extends Enemy {
+
+        update() {
+            this.timeStep += this.game.time.elapsedMS;
+            if (this.timeStep >= Enemy.FIXED_TIMESTEP) {
+                this.timeStep = this.timeStep % Enemy.FIXED_TIMESTEP; // save remainder
+
+                if ((this.animNum == 1 && this.frame === 2) || this.animNum == 2) {
+                    this.animNum = 2;
+                    this.frame = 0;
+                } else if (this.animNum == 0) {
+                    if (this.game.hero.x - this.x < 100) {
+                        this.animNum = 1;
+                        this.frame = 0;
+                    }
+                } else {
+                    this.animate();
+                }
+            }
+
+            this.drawEnemy();
+        }
+
+    }
+
+}
