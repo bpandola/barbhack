e43f3f6e236591bd9395b8c1b4d9019d3ba6c9a0
bpandola <brian.pandola@philips.com>
2016-09-05 16:29:29 -0700

    Initial commit for Level

---
+++ BarbConversionUtils/src/level.ts
@@ -0,0 +1,40 @@
+﻿namespace Barbarian {
+
+    export class Item {
+        id: number;
+        x: number;
+        y: number;
+        visible: boolean;
+        roomNum: number;
+
+        constructor(id: number, x: number, y:number, roomNum: number) {
+            this.id = id;
+            this.x = x;
+            this.y = y;
+            this.roomNum = roomNum;
+            this.visible = true;
+        }
+    }
+
+    export class Level {
+
+        private items: Item[] = [];
+
+        constructor(rawData: any) {
+            for (var room of rawData) {
+                for (var item of room.items) {
+                    this.items.push(new Item(item.id, item.x, item.y, room.id));
+                }
+            }
+
+        }
+
+        getRoomItems(room: number) {
+            return this.items.filter((item) => {
+                return item.roomNum == room && item.visible;
+            });
+        }
+    }
+
+
+}
