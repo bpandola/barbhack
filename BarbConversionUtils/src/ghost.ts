5d6cf7fea31220b633db4e82c152e1f168b2315b
bpandola <brian.pandola@philips.com>
2016-09-04 18:07:31 -0700

    Add ghost.ts for enemy kill sprite

---
+++ BarbConversionUtils/src/ghost.ts
@@ -0,0 +1,38 @@
+﻿namespace Barbarian {
+
+    export class Ghost extends Phaser.Sprite {
+
+        static FIXED_TIMESTEP: number = FIXED_TIMESTEP;
+
+        timeStep: number = 0;
+
+        constructor(game: Barbarian.Game, enemy: Barbarian.Enemies.Enemy) {
+            super(game, enemy.x, enemy.y, 'misc', 20);
+            var xAnchor = enemy.rotate ? 0 : 1;
+            this.anchor.setTo(xAnchor, 1);
+        }
+
+        update() {
+
+
+
+            this.timeStep += this.game.time.elapsedMS;
+            if (this.timeStep >= Ghost.FIXED_TIMESTEP) {
+                this.timeStep = this.timeStep % Ghost.FIXED_TIMESTEP; // save remainder
+
+                this.y -= TILE_SIZE;
+                this.frame = <number>this.frame + 1;
+
+                if (this.frame > 27)
+                    this.destroy();
+                
+            }
+
+
+        }
+
+    }
+
+
+
+}
