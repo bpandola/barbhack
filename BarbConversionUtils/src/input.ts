38b2587061fd4d66302d84799a149e80940aa67e
bpandola <brian.pandola@philips.com>
2018-07-22 02:29:01 -0700

    Lots of changes...

--- BarbConversionUtils/src/input.ts
+++ BarbConversionUtils/src/input.ts
@@ -11,28 +11,28 @@
         Attack = 128,
         Jump = 256,
         Stop = 512,
+        Get = 1024,
+        Run = 2048,
     }
 
-    export const NumIcons = 16;
-
-    export enum Icon {
-        Left,
-        Up,
-        Down,
-        Right,
-        Stop,
-        Jump,
-        Run,
-        Attack,
-        Defend,
-        Flee,
-        Get,
-        Use,  // It's 'Use' in the manual, but it's really just Stop on the secondary icon menu.
-        Drop,
-        Sword,
-        Bow,
-        Shield,
-        None,
+    export enum Icons {
+        None = 0,
+        Left = 8,
+        Up = 1,
+        Down = 2,
+        Right = 4,
+        Stop = 0,
+        Jump = 32,
+        Run = 64,
+        Attack = 16,
+        Defend = 128,
+        Flee = 256,
+        Get = 512,
+        Use = 1024,  // It's 'Use' in the manual, but it's really just Stop on the secondary icon menu.
+        Drop = 2048,
+        Sword = 4096,
+        Bow = 8192,
+        Shield = 16384
     }
 
     export class ControlCodes {
@@ -57,22 +57,15 @@
 
     export class HudState {
 
-        private iconsState: boolean[] = new Array(NumIcons);
+        private iconSelected: Icons;
         private iconState2: Buttons = 0;
 
-        constructor(iconsSelected: Icon[], iconState: number) {
-            this.iconState2 = iconState;
-            for (var i = 0; i < NumIcons; i++) {
-                this.iconsState[i] = iconsSelected.indexOf(i) == -1 ? false : true;
-            }
+        constructor(iconSelected: Icons) {
+            this.iconSelected = iconSelected;
         }
 
-        isIconSelected(icon: Icon): boolean {
-            return this.iconsState[icon];
-        }
-
-        isButtonSelected(button: Buttons): boolean {
-            return (this.iconState2 & button) > 0;
+        isIconSelected(icon: Icons): boolean {
+            return this.iconSelected == icon;
         }
 
     }
@@ -124,20 +117,45 @@
         public static UpRight: Buttons = ControlDirection.Up | ControlDirection.Right;
         public static DownLeft: Buttons = ControlDirection.Down | ControlDirection.Left;
         public static DownRight: Buttons = ControlDirection.Down | ControlDirection.Right;
+        public static Horizontal: Buttons = ControlDirection.Left | ControlDirection.Right;
+        public static Vertical: Buttons = ControlDirection.Up | ControlDirection.Down;
         public static Any: Buttons = ControlDirection.Up | ControlDirection.Down | ControlDirection.Left | ControlDirection.Right;
 
         static fromInput(keyboardState: KeyboardState, hudState: HudState): Buttons {
             var direction: Buttons = ControlDirection.None;
             // Get vertical direction
-            if (keyboardState.isKeyDown(ControlCodes.UP) || hudState.isButtonSelected(Buttons.Up) || hudState.isIconSelected(Icon.Up)) {
+            if (keyboardState.isKeyDown(ControlCodes.UP) || hudState.isIconSelected(Icons.Up)) {
                 direction |= ControlDirection.Up;
-            } else if (keyboardState.isKeyDown(ControlCodes.DOWN) || hudState.isButtonSelected(Buttons.Down) || hudState.isIconSelected(Icon.Down)) {
+            } else if (keyboardState.isKeyDown(ControlCodes.DOWN) || hudState.isIconSelected(Icons.Down)) {
                 direction |= ControlDirection.Down;
             }
             // Combine with horizontal direction
-            if (keyboardState.isKeyDown(ControlCodes.LEFT) || hudState.isButtonSelected(Buttons.Left) || hudState.isIconSelected(Icon.Left)) {
+            if (keyboardState.isKeyDown(ControlCodes.LEFT) || hudState.isIconSelected(Icons.Left)) {
                 direction |= ControlDirection.Left;
-            } else if (keyboardState.isKeyDown(ControlCodes.RIGHT) || hudState.isButtonSelected(Buttons.Right) || hudState.isIconSelected(Icon.Right)) {
+            } else if (keyboardState.isKeyDown(ControlCodes.RIGHT) || hudState.isIconSelected(Icons.Right)) {
+                direction |= ControlDirection.Right;
+            }
+
+            return direction;
+        }
+
+        static fromButtons(buttons: Buttons): Buttons {
+            // Extract the direction from a full set of buttons using a bit mask.
+            return buttons & ControlDirection.Any;
+        }
+
+        static fromIconState(iconState: HudState): Buttons {
+            var direction: Buttons = ControlDirection.None;
+            // Get vertical direction
+            if (iconState.isIconSelected(Icons.Up)) {
+                direction |= ControlDirection.Up;
+            } else if (iconState.isIconSelected(Icons.Down)) {
+                direction |= ControlDirection.Down;
+            }
+            // Combine with horizontal direction
+            if (iconState.isIconSelected(Icons.Left)) {
+                direction |= ControlDirection.Left;
+            } else if (iconState.isIconSelected(Icons.Right)) {
                 direction |= ControlDirection.Right;
             }
 
@@ -170,7 +188,7 @@
         /// <summary>
         /// Provides the map of non-direction buttons to keyboard keys and hud icons.
         /// </summary>
-        private nonDirectionButtons: { [key: string]: { button: Buttons, controlKey: number, icon: Icon; } } = {};
+        private nonDirectionButtons: { [key: string]: { button: Buttons, controlKey: number, icon: Icons; } } = {};
 
         constructor(game: Barbarian.Game, hud: Hud) {
             this.game = game;
@@ -196,11 +214,12 @@
 
             //this.nonDirectionButtons[Buttons.A.toString()] = { button: Buttons.A, controlKey: ControlCodes.A };
             //this.nonDirectionButtons[Buttons.B.toString()] = { button: Buttons.B, controlKey: ControlCodes.B };
-            this.nonDirectionButtons[Buttons.Stop.toString()] = { button: Buttons.Stop, controlKey: ControlCodes.F5, icon: Icon.Stop };
-            this.nonDirectionButtons[Buttons.Jump.toString()] = { button: Buttons.Jump, controlKey: ControlCodes.F6, icon: Icon.Jump };
-            this.nonDirectionButtons[Buttons.Attack.toString()] = { button: Buttons.Attack, controlKey: ControlCodes.F8, icon: Icon.Attack };
-            this.nonDirectionButtons[Buttons.Flee.toString()] = { button: Buttons.Flee, controlKey: ControlCodes.F10, icon: Icon.Flee };
-
+            this.nonDirectionButtons[Buttons.Stop.toString()] = { button: Buttons.Stop, controlKey: ControlCodes.F5, icon: Icons.Stop };
+            this.nonDirectionButtons[Buttons.Jump.toString()] = { button: Buttons.Jump, controlKey: ControlCodes.F6, icon: Icons.Jump };
+            this.nonDirectionButtons[Buttons.Attack.toString()] = { button: Buttons.Attack, controlKey: ControlCodes.F8, icon: Icons.Attack };
+            this.nonDirectionButtons[Buttons.Flee.toString()] = { button: Buttons.Flee, controlKey: ControlCodes.F10, icon: Icons.Flee };
+            this.nonDirectionButtons[Buttons.Get.toString()] = { button: Buttons.Get, controlKey: ControlCodes.DOWN, icon: Icons.Get };
+            this.nonDirectionButtons[Buttons.Run.toString()] = { button: Buttons.Run, controlKey: ControlCodes.F7, icon: Icons.Run };
         }
 
         update(gameTime: Phaser.Time): void {
@@ -228,7 +247,7 @@
             for (var key in this.nonDirectionButtons) {
                 var button: Buttons = this.nonDirectionButtons[key].button;
                 var controlKey: number = this.nonDirectionButtons[key].controlKey;
-                var icon: Icon = this.nonDirectionButtons[key].icon;
+                var icon: Icons = this.nonDirectionButtons[key].icon;
                 // Check if just pressed
                 if ((this.lastKeyboardState.isKeyUp(controlKey) && this.keyboardState.isKeyDown(controlKey))
                     || (!this.lastHudState.isIconSelected(icon) && this.hudState.isIconSelected(icon))
@@ -254,6 +273,13 @@
                 mergeInput = false;
             //}
 
+            // HACK - Merge previous hud state for up/down and left/right.
+                if (direction & ControlDirection.Horizontal) {
+                    direction |= (ControlDirection.fromIconState(this.lastHudState) & ControlDirection.Vertical)
+                } else if (direction & ControlDirection.Vertical) {
+                    direction |= (ControlDirection.fromIconState(this.lastHudState) & ControlDirection.Horizontal)
+                }
+            
             // If there was any new input on this update, add it to our buffer.
             if (buttons != 0) {
                 if (mergeInput) {
