c11d4e082c6fecb0847f628efa8c491d63f407ea
bpandola <brian.pandola@philips.com>
2018-08-05 21:10:27 -0700

    Working before major change...

--- BarbConversionUtils/src/test.ts
+++ BarbConversionUtils/src/test.ts
@@ -83,7 +83,93 @@
         }
     }
 
-    class TestEntity extends Entity {
+    export class GameEntity extends Phaser.Sprite {
+
+        timeStep: number = 0;
+
+        facing: Facing;      // Which way the entity is facing (left or right).
+        direction: Direction;   // Which way the entity is moving (up, down, left, right).
+
+        group: Phaser.Group;
+
+        constructor(game: Barbarian.Game, key: string) {
+            super(game, 0, 0, 'NLL');
+            // Add all parts to the group
+            this.group = new Phaser.Group(game, this);
+            for (var i = 0; i < game.cache.getFrameCount(key); i++) {
+                var part = <Phaser.Sprite>this.group.create(0, 0, key, i);
+                // Set default properties.
+                part.anchor.setTo(0.5);
+                part.visible = false;
+            }
+        }
+
+        // Should be overridden by subclasses
+        get currentParts() {
+            return [];
+        }
+
+        // arguments can be decimals, e.g. 0.5 for a half-tile movement.
+        moveRelative(numTilesX: number, numTilesY: number): void {
+
+            var xMovement = this.facing == Facing.Right ? TILE_SIZE : -TILE_SIZE;
+            var yMovement = this.direction == Direction.Up ? -TILE_SIZE : TILE_SIZE;
+
+            this.x += xMovement * numTilesX;
+            this.y += yMovement * numTilesY;
+
+        }
+
+        update() {
+            // Call derived objects' tick function on a fixed timebase.
+            this.timeStep += this.game.time.elapsedMS;
+            if (this.timeStep >= FIXED_TIMESTEP) {
+                this.timeStep = this.timeStep % FIXED_TIMESTEP;
+                this.tick()
+            }
+        }
+
+        // Should be overridden by subclasses
+        tick() { }
+
+        render() {
+            // Start from scratch every time.
+            this.group.forEach((part) => { part.visible = false; }, this);
+            // For each currently visible part, reset properties and set visible to true.
+            for (var i = 0, parts = this.currentParts; i < parts.length; i++) {
+                // Get a reference to the sprite for this part.
+                var part = parts[i];
+                // We have to ignore the arrow part...
+                if (part.index < 0) {
+                    continue;
+                }
+                var spr = <Phaser.Sprite>this.group.getChildAt(part.index);
+                // Need to reset the scale here or we may get negative sprite width/height,
+                // which will throw off the x/y calculations.
+                // {@link https://github.com/photonstorm/phaser/issues/1210 Issue}
+                spr.scale.setTo(1, 1);
+                // Reset coordinates for this part.
+                spr.x = this.facing === Facing.Left ? part.rx : part.x;
+                spr.y = this.facing === Facing.Left ? part.ry : part.y;
+                // Translate Barbarian x/y data for use with a Phaser middle anchor.
+                spr.x += spr.width / 2;
+                spr.y += spr.height / 2;
+                // Set z relative to when the part was added for proper overlap.
+                spr.z = i;
+                // Part bit flags specify whether to flip the sprite part horizontally or vertically.
+                var xScale = part.flags & 1 ? -1 : 1;
+                var yScale = part.flags & 2 ? -1 : 1;
+                // Flip the part horizontally again based on which way the *entity* is facing.
+                xScale = this.facing === Facing.Left ? -xScale : xScale
+                spr.scale.setTo(xScale, yScale);
+                // And finally, make sure this sprite part is rendered by Phaser.
+                spr.visible = true;
+            }
+        }
+
+    }
+
+    class TestEntity extends GameEntity {
 
         game: Barbarian.Game;
         fakeSprite: Phaser.Sprite;
@@ -114,7 +200,7 @@
     }
 
 
-    class TestHero extends Entity {
+    class TestHero extends GameEntity {
 
         game: Barbarian.Game;
         fakeSprite: Phaser.Sprite;
@@ -234,7 +320,7 @@
 
         update() {
 
-         
+            this.enemyLeft.group.x += .1;
 
 
         }
